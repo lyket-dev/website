b39576a05e7957500304c5370e0753fa99fbcbab
Irene Oppo <i.oppo@datocms.com>
2020-08-04 10:10:44 +0200

    schema

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import useAsyncEffect from "../utils/useAsyncEffect";
 // import { fetchDocs } from "../api";
 import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
 export default function Docs() {
   useAsyncEffect(() => {
@@ -13,7 +14,7 @@ export default function Docs() {
       <section className="section">
         <div className="section__container">
           <p className="section__title">Documentation</p>
-          <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+          <SwaggerUI url="http://localhost:3000/schema" />
         </div>
       </section>
     </div>
