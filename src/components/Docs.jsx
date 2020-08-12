ec2a9f99d5f674f23494476d0a4fe0b0f237eec0
Irene Oppo <i.oppo@datocms.com>
2020-08-12 19:49:34 +0200

    markdown

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import useAsyncEffect from "../utils/useAsyncEffect";
 // import { fetchDocs } from "../api";
 import SwaggerUI from "swagger-ui-react";
+import ReactMarkdown from "react-markdown";
 import "swagger-ui-react/swagger-ui.css";
 
 export default function Docs() {
+  const [markdown, setMarkdown] = useState(null);
+
   useAsyncEffect(() => {
-    // await fetchDocs();
+    fetch("https://raw.githubusercontent.com/lyket-dev/react/master/README.md")
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
   }, []);
 
   return (
     <div className="Page">
-      <section className="section">
+      <section className="section--left">
         <div className="section__container">
           <p className="section__title">React component Documentation</p>
         </div>
+        <ReactMarkdown source={markdown} />
       </section>
       <section className="section">
         <div className="section__container">
