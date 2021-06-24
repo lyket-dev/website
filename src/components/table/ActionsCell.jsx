20df6b862de96d41f6b3126e3ba12ed4c8c6a181
Irene Oppo <i.oppo@datocms.com>
2021-06-24 12:41:05 +0200

    Add showConfirm

--- src/components/table/ActionsCell.jsx
+++ src/components/table/ActionsCell.jsx
@@ -10,12 +10,36 @@ import { ReactComponent as Destroy } from "assets/icons/outline/trash.svg";
 import { ReactComponent as Reset } from "assets/icons/outline/rewind.svg";
 import Tooltip from "components/Tooltip";
 import { notice, alert } from "utils/notifications";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 
 export default function ActionsCell({ buttonId }) {
   const dispatch = useDispatch();
 
+  const confirmPromise = async ({ title, message }) =>
+    new Promise((resolve, _reject) => {
+      confirmAlert({
+        title: title || "Are you sure you want to proceed?",
+        message: message || "Click yes to proceed",
+        buttons: [
+          {
+            label: "Yes",
+            onClick: resolve,
+          },
+          {
+            label: "No",
+            onClick: () => {},
+          },
+        ],
+      });
+    });
+
   const handleDestroyButton = async () => {
     try {
+      await confirmPromise({
+        title: "Are you sure you want to destroy this button?",
+      });
+
       await dispatch(destroyButton(buttonId));
       notice({ message: "Button destroyed successfully" });
     } catch (error) {
@@ -27,8 +51,21 @@ export default function ActionsCell({ buttonId }) {
     }
   };
 
-  const handleResetButton = () => {
-    return dispatch(resetButton(buttonId)).then(() => dispatch(fetchAll()));
+  const handleResetButton = async () => {
+    try {
+      await confirmPromise({
+        title: "Are you sure you want to reset the counter?",
+        message: "This button's counter will be set to 0",
+      });
+      await dispatch(resetButton(buttonId)).then(() => dispatch(fetchAll()));
+      notice({ message: "Button reset successfully" });
+    } catch (error) {
+      alert({
+        message: `Could not reset button ${
+          error.errors[0] && error.errors[0].message
+        }`,
+      });
+    }
   };
 
   return (
