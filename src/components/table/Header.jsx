7e5a42c6b61de383ea2aa16f9805cc845e0da192
Irene Oppo <i.oppo@datocms.com>
2022-12-15 21:26:45 +0100

    fix-build

--- src/components/table/Header.jsx
+++ src/components/table/Header.jsx
@@ -1,40 +1,48 @@
 import React from "react";
-import humanizeString from "humanize-string";
 import { Link } from "react-router-dom";
 
+function humanize(str) {
+	return str
+		.replace(/^[\s_]+|[\s_]+$/g, "")
+		.replace(/[_-\s]+/g, " ")
+		.replace(/^[a-z]/, function (m) {
+			return m.toUpperCase();
+		});
+}
+
 export default function Header({ namespace, currentType, icons }) {
-  return (
-    <>
-      <h2 className="pane__title">
-        {namespace
-          ? humanizeString(`${namespace} ${currentType} buttons`)
-          : `All ${currentType} buttons`}
-      </h2>
-      <div className="type-menu">
-        <Link
-          to={`/dashboard/like${namespace ? `/${namespace}` : ""}`}
-          className="type-menu__link"
-        >
-          {icons["like"]}
-          Like Buttons
-        </Link>
-        <span>|</span>
-        <Link
-          to={`/dashboard/updown${namespace ? `/${namespace}` : ""}`}
-          className="type-menu__link"
-        >
-          {icons["updown"]}
-          Like/Dislike Buttons
-        </Link>
-        <span>|</span>
-        <Link
-          to={`/dashboard/clap${namespace ? `/${namespace}` : ""}`}
-          className="type-menu__link"
-        >
-          {icons["clap"]}
-          Clap Buttons
-        </Link>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h2 className="pane__title">
+				{namespace
+					? humanize(`${namespace} ${currentType} buttons`)
+					: `All ${currentType} buttons`}
+			</h2>
+			<div className="type-menu">
+				<Link
+					to={`/dashboard/like${namespace ? `/${namespace}` : ""}`}
+					className="type-menu__link"
+				>
+					{icons["like"]}
+					Like Buttons
+				</Link>
+				<span>|</span>
+				<Link
+					to={`/dashboard/updown${namespace ? `/${namespace}` : ""}`}
+					className="type-menu__link"
+				>
+					{icons["updown"]}
+					Like/Dislike Buttons
+				</Link>
+				<span>|</span>
+				<Link
+					to={`/dashboard/clap${namespace ? `/${namespace}` : ""}`}
+					className="type-menu__link"
+				>
+					{icons["clap"]}
+					Clap Buttons
+				</Link>
+			</div>
+		</>
+	);
 }
--- src/utils/createEntityReducer.js
+++ src/utils/createEntityReducer.js
@@ -1,64 +1,64 @@
 import omit from "object.omit";
 
 const mergeIntoState = function mergeIntoState(type, resources, state) {
-  return resources
-    .filter((resource) => resource.type === type)
-    .reduce((acc, resource) => ({ ...acc, [resource.id]: resource }), state);
+	return resources
+		.filter((resource) => resource.type === type)
+		.reduce((acc, resource) => ({ ...acc, [resource.id]: resource }), state);
 };
 
 function mergeEntities(type, state, { payload }) {
-  if (!payload || !payload.response) {
-    return state;
-  }
+	if (!payload?.response) {
+		return state;
+	}
 
-  const { response } = payload;
-  let newState = {};
+	const { response } = payload;
+	let newState = {};
 
-  if (response.included) {
-    newState = mergeIntoState(type, response.included, newState);
-  }
+	if (response.included) {
+		newState = mergeIntoState(type, response.included, newState);
+	}
 
-  if (Array.isArray(response.data)) {
-    newState = mergeIntoState(type, response.data, newState);
-  }
+	if (Array.isArray(response.data)) {
+		newState = mergeIntoState(type, response.data, newState);
+	}
 
-  if (response.data) {
-    newState = mergeIntoState(type, [response.data], newState);
-  }
+	if (response.data) {
+		newState = mergeIntoState(type, [response.data], newState);
+	}
 
-  if (payload.force && payload.query && payload.query.include) {
-    const included = payload.query.include
-      .split(",")
-      .map((relationship) => relationship.replace(/(^[^.]+.|s$)/g, ""));
+	if (payload.force && payload.query && payload.query.include) {
+		const included = payload.query.include
+			.split(",")
+			.map((relationship) => relationship.replace(/(^[^.]+.|s$)/g, ""));
 
-    if (included.includes(type)) {
-      return newState;
-    }
-  }
+		if (included.includes(type)) {
+			return newState;
+		}
+	}
 
-  return {
-    ...state,
-    ...newState,
-  };
+	return {
+		...state,
+		...newState,
+	};
 }
 
 function manageDestroyAction(actionCreator, state, action) {
-  if (actionCreator.toString() === action.type) {
-    return omit(state, [action.payload.response.data.id]);
-  }
+	if (actionCreator.toString() === action.type) {
+		return omit(state, [action.payload.response.data.id]);
+	}
 
-  return state;
+	return state;
 }
 
-export default function (type, destroyActionCreator = null) {
-  const initialState = {};
-  return (state = initialState, action) => {
-    let newState = mergeEntities(type, state, action);
+export default function createEntityReducer(type, destroyActionCreator = null) {
+	const initialState = {};
+	return (state = initialState, action) => {
+		let newState = mergeEntities(type, state, action);
 
-    if (destroyActionCreator) {
-      newState = manageDestroyAction(destroyActionCreator, newState, action);
-    }
+		if (destroyActionCreator) {
+			newState = manageDestroyAction(destroyActionCreator, newState, action);
+		}
 
-    return newState;
-  };
+		return newState;
+	};
 }
